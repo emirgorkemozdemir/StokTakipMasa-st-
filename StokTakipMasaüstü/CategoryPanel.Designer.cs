3cf0111a5eb300d8af3cd4b6e4f609682891dbd8
emirgorkemozdemir <60661125+emirgorkemozdemir@users.noreply.github.com>
2025-01-19 23:55:13 +0300

    Implemented add, delete and list operations for categories. Created a method for list operations and used it within add and delete.

--- StokTakipMasaüstü/CategoryPanel.Designer.cs
+++ StokTakipMasaüstü/CategoryPanel.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.dgrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tboxAddName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,23 +44,80 @@
             this.dgrid.Name = "dgrid";
             this.dgrid.Size = new System.Drawing.Size(742, 215);
             this.dgrid.TabIndex = 0;
+            this.dgrid.SelectionChanged += new System.EventHandler(this.dgrid_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kategori Adı Giriniz";
+            // 
+            // tboxAddName
+            // 
+            this.tboxAddName.Location = new System.Drawing.Point(12, 279);
+            this.tboxAddName.Name = "tboxAddName";
+            this.tboxAddName.Size = new System.Drawing.Size(100, 20);
+            this.tboxAddName.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 314);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kategori ID\'sini Giriniz";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(188, 277);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // CategoryPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(767, 450);
+            this.ClientSize = new System.Drawing.Size(767, 347);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tboxAddName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgrid);
             this.Name = "CategoryPanel";
             this.Text = "CategoryPanel";
             this.Load += new System.EventHandler(this.CategoryPanel_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tboxAddName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
--- StokTakipMasaüstü/CategoryPanel.cs
+++ StokTakipMasaüstü/CategoryPanel.cs
@@ -18,14 +18,14 @@ namespace StokTakipMasaüstü
             InitializeComponent();
         }
 
-        private void CategoryPanel_Load(object sender, EventArgs e)
+        public void KategorileriYukle()
         {
             // En başta baglantıyı kontrol etmem lazım
             SqlConnectionClass.CheckConnection();
 
             // CategoryPanel yüklendiginde buradaki işlemler gerçekleşecek. 
             // SqlCommand içerisine baglantımızı attık
-            SqlCommand command_load = new SqlCommand("SELECT * FROM TableCategory",SqlConnectionClass.myconnection);
+            SqlCommand command_load = new SqlCommand("SELECT * FROM TableCategory", SqlConnectionClass.myconnection);
 
             // SQL Komutunu çalıştırıp verileri ekrana almam lazım.
             SqlDataAdapter dataAdapter = new SqlDataAdapter(command_load);
@@ -38,6 +38,53 @@ namespace StokTakipMasaüstü
 
             // datagrid'e verileri yansıttım.
             dgrid.DataSource = data_table;
+        }
+
+        private void CategoryPanel_Load(object sender, EventArgs e)
+        {
+            KategorileriYukle();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // En başta baglantıyı kontrol etmem lazım
+            SqlConnectionClass.CheckConnection();
+
+            // SqlCommand içerisine ekleme kodumuzu yazıp baglantımızı atıyoruz
+            SqlCommand command_add= new SqlCommand("INSERT INTO TableCategory (CategoryName) VALUES (@pname)", SqlConnectionClass.myconnection);
+
+            // Açtıgım parametreye deger veriyorum
+            command_add.Parameters.AddWithValue("@pname",tboxAddName.Text);
+
+            // Komutu çalıştırıyorum
+            command_add.ExecuteNonQuery();
+
+            // Ekrandaki verileri tekrar yüklüyorum
+            KategorileriYukle();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // En başta baglantıyı kontrol etmem lazım
+            SqlConnectionClass.CheckConnection();
+
+            // SqlCommand içerisine silme kodumuzu yazıyourz
+            SqlCommand command_delete = new SqlCommand("DELETE FROM TableCategory WHERE CategoryID=@pid", SqlConnectionClass.myconnection);
+
+            // Komuttaki parametreye deger veriyorum
+            command_delete.Parameters.AddWithValue("@pid",secili_id);
+
+            // Komutu çalıştırıyorum
+            command_delete.ExecuteNonQuery();
+
+            // Ekrandaki verileri tekrar yüklüyorum
+            KategorileriYukle();
+        }
+
+        int secili_id = 0;
+        private void dgrid_SelectionChanged(object sender, EventArgs e)
+        {
+            secili_id = Convert.ToInt32(dgrid.CurrentRow.Cells[0].Value);
         }
     }
 }
